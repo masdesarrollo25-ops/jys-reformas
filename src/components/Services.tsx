063f74c6e6753ba7daf5199242f61209071d1a65
Mas Desarrollo <masdesarrollo25@gmail.com>
2026-04-03 20:09:08 +0200

    BD produccion

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -45,7 +45,7 @@ export default function Services() {
         </svg>
       )
     },
-    {
+    /*{
       title: "Cerrajería",
       desc: "Instalación de cerraduras de seguridad, bombines y apertura de puertas trabadas.",
       icon: (
@@ -53,7 +53,7 @@ export default function Services() {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
         </svg>
       )
-    }
+    }*/
   ];
 
   return (
